92f9f699c38d46bd2b9826d0aad11d874461f07e
Detrivinog <detrivinog@gmail.com>
2022-08-08 21:53:46 -0500

    Clase 4: iniciar proyecto

---
+++ mi-proyecto/src/App.js
@@ -0,0 +1,14 @@
+import satisfied from './satisfied.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={satisfied} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
